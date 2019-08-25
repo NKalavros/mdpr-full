aec5fdfa3870e1e607fcf93ac302637302dd1ab7
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-25 21:49:31 +0300

    Update qrnas.sh
    Created an alias to use for the config file during the execution of the shell scripts

--- qrnas.sh
+++ qrnas.sh
@@ -6,7 +6,11 @@ tar -xvzf QRNAS.tar.gz
 rm -r QRNAS.tar.gz
 cd QRNAS
 sudo make parallel
-echo export PATH='$PATH':$(pwd) >>~/.bashrc
+sudo sed -i "s/#WRITEFREQ  1000/WRITEFREQ  1000/" configfile.txt
+sudo sed -i "s/STEPS     5000   # Maximal number of steps; by default 100000/STEPS     10000/" configfile.txt
+sudo sed -i "s/NUMTHREADS  08      # Number of threads (parallel builds only!); by default 4/NUMTHREADS  80/" configfile.txt
+sudo cp ./configfile.txt /usr/local/bin/configfile.txt
+echo "alias qrnaconfig="cat /usr/local/bin/configfile.txt"" >> ~/.bashrc
 echo export QRNAS_FF_DIR=$(pwd)/forcefield >>~/.bashrc
 source ~/.bashrc
 cd ..   
