cff6c06b9d6d2de6b743e4604d768745b52614cb
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-26 00:09:01 +0300

    Update install_haddock.sh

--- install_haddock.sh
+++ install_haddock.sh
@@ -84,9 +84,9 @@ sudo sed -i "s|###export OMP_NUM_THREADS 4|export OMP_NUM_THREADS=$NPROC|" ./.cn
 source ./.cns_solve_env_sh
 sudo make install
 cd ../haddock-deps/haddock2.2
-sudo sed -i "s/{===>} structures_0=1000;/{===>} structures_0=500;/" ./protocols/run.cns
-sudo sed -i "s/{===>} structures_1=200;/{===>} structures_1=100;/" ./protocols/run.cns
-sudo sed -i "s/{===>} structures_1=200;/{===>} structures_1=100;/" ./protocols/run.cns
+sudo sed -i "s/{===>} structures_0=1000;/{===>} structures_0=480;/" ./protocols/run.cns
+sudo sed -i "s/{===>} structures_1=200;/{===>} structures_1=80;/" ./protocols/run.cns
+sudo sed -i "s/{===>} structures_1=200;/{===>} structures_1=80;/" ./protocols/run.cns
 cd ..
 cd haddock2.2/examples/protein-dna
 haddock2.2
