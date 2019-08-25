2ebd41a87914301c3320762e80c1766f1cd05def
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-25 21:58:07 +0300

    Update qrnas.sh

--- qrnas.sh
+++ qrnas.sh
@@ -7,8 +7,10 @@ rm -r QRNAS.tar.gz
 cd QRNAS
 sudo make parallel
 sudo sed -i "s/#WRITEFREQ  1000/WRITEFREQ  1000/" configfile.txt
-sudo sed -i "s/STEPS     5000   # Maximal number of steps; by default 100000/STEPS     10000/" configfile.txt
-sudo sed -i "s/NUMTHREADS  08      # Number of threads (parallel builds only!); by default 4/NUMTHREADS  80/" configfile.txt
+sudo sed -i "s/STEPS     5000/STEPS     10000/" configfile.txt
+sudo sed -i "s/NUMTHREADS  08/NUMTHREADS  80/" configfile.txt
+sudo sed -i "s/#HBONDS     0/HBONDS     0/" configfile.txt
+sudo sed -i "s/#SSDETECT   0/SSDETECT   0/" configfile.txt
 sudo cp ./configfile.txt /usr/local/bin/configfile.txt
 echo "alias qrnaconfig="cat /usr/local/bin/configfile.txt"" >> ~/.bashrc
 echo export QRNAS_FF_DIR=$(pwd)/forcefield >>~/.bashrc
