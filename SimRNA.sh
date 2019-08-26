c542ea0a85c86d792f50af1fb6522fa7d738fc11
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-27 01:04:44 +0300

    Update SimRNA.sh
    Some file linkings only

--- SimRNA.sh
+++ SimRNA.sh
@@ -6,10 +6,13 @@ tar -zxvf SimRNA_64bitIntel_Linux.tgz
 rm -r SimRNA_64bitIntel_Linux.tgz
 cd SimRNA_64bitIntel_Linux
 sudo ln -s $(pwd)/* /usr/local/bin
-#echo "CGCUUCAUAUAAUCCUAAUGAUAUGGUUUGGGAGUUUCUACCAAGAGCCUUAAACUCUUGAUUAUGAAGUG" > test_seq.fa
-#echo "(((((((((...((((((.........))))))........((((((.......))))))..)))))))))" > test_seq_secstr.fa
 sed -i -e 's/NUMBER_OF_ITERATIONS 16000000/NUMBER_OF_ITERATIONS 800000/g' config.dat
 sed -i -e "s/TRA_WRITE_IN_EVERY_N_ITERATIONS 1600/TRA_WRITE_IN_EVERY_N_ITERATIONS 800" config.dat
+sudo ln -s ./data ..
+cp config.dat ../config.dat
+#Example script, not ran!
+#echo "CGCUUCAUAUAAUCCUAAUGAUAUGGUUUGGGAGUUUCUACCAAGAGCCUUAAACUCUUGAUUAUGAAGUG" > test_seq.fa
+#echo "(((((((((...((((((.........))))))........((((((.......))))))..)))))))))" > test_seq_secstr.fa
 #for i in {1..10};
 #do
 #    ./SimRNA -s test_seq.fa -S test_seq_secstr.fa -c config.dat -R 1000 -E 8 -o fold_test_seq_$i >& fold_test_seq_$i.log & 
