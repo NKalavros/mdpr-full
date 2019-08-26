c7d21723c4b7897381dc87a622dab7e12c9fffac
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-26 04:08:10 +0300

    Update simrna.sh

--- simrna.sh
+++ simrna.sh
@@ -6,22 +6,22 @@ tar -zxvf SimRNA_64bitIntel_Linux.tgz
 rm -r SimRNA_64bitIntel_Linux.tgz
 cd SimRNA_64bitIntel_Linux
 sudo ln -s $(pwd)/* /usr/local/bin
-echo "CGCUUCAUAUAAUCCUAAUGAUAUGGUUUGGGAGUUUCUACCAAGAGCCUUAAACUCUUGAUUAUGAAGUG" > test_seq.fa
-echo "(((((((((...((((((.........))))))........((((((.......))))))..)))))))))" > test_seq_secstr.fa
+#echo "CGCUUCAUAUAAUCCUAAUGAUAUGGUUUGGGAGUUUCUACCAAGAGCCUUAAACUCUUGAUUAUGAAGUG" > test_seq.fa
+#echo "(((((((((...((((((.........))))))........((((((.......))))))..)))))))))" > test_seq_secstr.fa
 sed -i -e 's/NUMBER_OF_ITERATIONS 16000000/NUMBER_OF_ITERATIONS 160000/g' config.dat
-for i in {1..10};
-do
-    ./SimRNA -s test_seq.fa -S test_seq_secstr.fa -c config.dat -R 1000 -E 8 -o fold_test_seq_$i >& fold_test_seq_$i.log & 
-done
-cat fold_test_seq_?_??.trafl > fold_test_seq_all.trafl
-./clustering fold_test_seq_all.trafl 0.01 2.5 >& fold_test_seq_all.log
-for i in fold_test_seq_all_thrs2.50A*.trafl;
-do
-    echo $i
-    SimRNA_trafl2pdbs fold_test_seq_1_01-000001.pdb $i 1 AA
-done
-secstr=`cat test_seq_secstr.fa`
-qrnaconfig >> qrnaconfig.txt
-sudo sed -i "s/#SECSTRUCT   (((....)))/SECSTRUCT $secstr/" qrnaconfig.txt
-QRNA -i fold_test_seq_1_01-000001.pdb -o final_seq.pdb -c qrnaconfig.txt
-rm *test*
+#for i in {1..10};
+#do
+#    ./SimRNA -s test_seq.fa -S test_seq_secstr.fa -c config.dat -R 1000 -E 8 -o fold_test_seq_$i >& fold_test_seq_$i.log & 
+#done
+#cat fold_test_seq_?_??.trafl > fold_test_seq_all.trafl
+#./clustering fold_test_seq_all.trafl 0.01 2.5 >& fold_test_seq_all.log
+#for i in fold_test_seq_all_thrs2.50A*.trafl;
+#do
+#    echo $i
+#    SimRNA_trafl2pdbs fold_test_seq_1_01-000001.pdb $i 1 AA
+#done
+#secstr=`cat test_seq_secstr.fa`
+#qrnaconfig >> qrnaconfig.txt
+#sudo sed -i "s/#SECSTRUCT   (((....)))/SECSTRUCT $secstr/" qrnaconfig.txt
+#QRNA -i fold_test_seq_1_01-000001.pdb -o final_seq.pdb -c qrnaconfig.txt
+#rm *test*
