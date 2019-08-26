829d608914dd071ed07d3794f77ad2a8faa28c16
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-27 02:06:14 +0300

    Update viennarna.sh

--- viennarna.sh
+++ viennarna.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install build-essential python python-dev python3 python3-dev python-pip python3-pip
-sudo -H pip install setuptools wheel
-sudo -H pip3 install setuptools wheel
+sudo -H pip install setuptools wheel numpy
+sudo -H pip3 install setuptools wheel numpy
 wget https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.14.tar.gz
 tar -zxvf ViennaRNA-2.4.14.tar.gz
 rm -r ViennaRNA-2.4.14.tar.gz
