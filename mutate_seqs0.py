de20f853925207adabf5058835084ff52b6a2c73
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-30 00:32:04 +0300

    Update mutate_seqs0.py

--- mutate_seqs0.py
+++ mutate_seqs0.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
-
-
 #Importing the needed packages
-iimport os
+import os
 import sys 
 import shutil
 import pprint
@@ -32,6 +29,14 @@ password ='' #Insert your own PC's password here, if it has one, leave it blank 
 cns_exec = "/root/haddock-deps/haddock2.2/../../cns_solve_1.3/intel-x86_64bit-linux/bin/cns"
 cores = "80"
 ####Make the mutation into a function
+def get_max_index():
+    fasta_max = [] #Get the max index of the filenames, in order to not calculate more things than needed
+    for filename in os.listdir(cwd): #Iterate over all files in directory
+        if filename.endswith(".fasta"): #Choose only fasta files
+            fasta_max.append(int(filename.replace(".fasta",""))) #Append filenames
+    fasta_max = max(fasta_max) #Obtain the maximum index
+    return(fasta_max)
+
 def mutate_seq_and_get_secondary_structure(filename,fasta_max):
     #Bases we have, the base_dict and its probability distribution
     bases = np.array(['A', 'U', 'G', 'C']) #Dictionary
@@ -43,14 +48,14 @@ def mutate_seq_and_get_secondary_structure(filename,fasta_max):
                              [0.05, 0.1, 0.05, 0.8]])
     parent_fasta = filename.replace(".fasta","")
     new_fasta_idx = str(int(fasta_max)+1)
-    fasta_secstr_filename = fasta_idx + ".secstr"
+    fasta_secstr_filename = new_fasta_idx + ".secstr"
     with open(filename,"r+") as f: #Open them
         fasta_seq = f.read().splitlines()[1] #Obtain sequences
     mutated_seq = "" #Empty string that will contain the new sequence
     number_of_mutations = 0 #Flag to keep the number of mutation
     index_to_start_mutating_from = np.random.randint(0,len(fasta_seq)) #Create the starting index
     for base in fasta_seq[index_to_start_mutating_from:]: #Index the sequence
-        new_base =  np.random.choice(bases, p = prob_distrib[base_dict[base]]) #Create the new base
+        new_base =  np.random.choice(bases, p = mutation_matrix[base_dict[base]]) #Create the new base
         if base != new_base: #Implement scaling mutation rate, if the new base is different
             number_of_mutations += 1 #Increasing the number of mutations
             diag = mutation_matrix[np.diag_indices_from(mutation_matrix)] #Store diagonal
@@ -63,36 +68,25 @@ def mutate_seq_and_get_secondary_structure(filename,fasta_max):
     (ss, mfe) = RNA.fold(mutated_seq) #Calculate the secondary structure
     new_fasta_filename = str(new_fasta_idx) + ".fasta" #Create new filenames
     new_fasta_secstr_filename = str(new_fasta_idx) + ".secstr" #Same for the secondary structure file
-    with open(fasta_max_filename,"w+") as f: #Open up a new file
+    with open(new_fasta_filename,"w+") as f: #Open up a new file
         f.write(">" + str(new_fasta_idx) + " parent: " + str(parent_fasta)) #Write the index and the parent
         f.write("\n") #Write a newline
         f.write(mutated_seq) #Write the actual sequence
-    with open(fasta_max_secstr_filename,"w+") as f: #Do the exact same for the secondary structure file
+    with open(new_fasta_secstr_filename,"w+") as f: #Do the exact same for the secondary structure file
         f.write(">" + str(new_fasta_idx) + " " + str(mfe) + " parent: " + str(parent_fasta)) #Write index, minimal folding energy and parent
         f.write("\n")
         f.write(ss)
     return(None)
 
-def get_max_index():
-    fasta_max = [] #Get the max index of the filenames, in order to not calculate more things than needed
-    for filename in os.listdir(cwd): #Iterate over all files in directory
-        if filename.endswith(".fasta"): #Choose only fasta files
-            fasta_filenames.append(filename.replace(".fasta","")) #Append filenames
-    fasta_max = max(fasta_max) #Obtain the maximum index
-    return(fasta_max)
-# In[13]:
-
 #Lets write a function for this
-def rna_tertiary_structure_prediction(filename):
-    with open(filename + "mpdr-rna.log","w") as main_logfile:
-        fasta_idx = filename.replace(".fasta","") #Get index
-        fasta_seq_filename = fasta_idx + ".fasta"
-        fasta_seq_simrna_filename = fasta_idx + "fasta.simrna" #Create dummy file for simRNA
-        fasta_secstr_filename = fasta_idx + ".secstr" #Get secondary structure (it must exist)
-        fasta_secstr_simrna_filename = fasta_idx + ".secstr.simrna"
-        fasta_terstr_filename = fasta_idx + ".pdb.simrna" #Get tertiary structure filename (will be created)
-        fasta_terstr_logfile_filename = fasta_idx + "tertiary_pred.log.simrna" #Create a logfile for the tertiary structure prediction
-        fasta_clustering_logfile_filename = fasta_idx + "clustering.log.simrna" #Create a logfile for the clustering procedure
+def rna_tertiary_structure_prediction(filename,angstrom_cutoff = "4.4",fraction_to_cluster = "0.01"):
+    fasta_idx = filename.replace(".fasta","") #Get index
+    fasta_seq_filename = fasta_idx + ".fasta"
+    fasta_seq_simrna_filename = fasta_idx + "fasta.simrna" #Create dummy file for simRNA
+    fasta_secstr_filename = fasta_idx + ".secstr" #Get secondary structure (it must exist)
+    fasta_secstr_simrna_filename = fasta_idx + ".secstr.simrna"
+    fasta_terstr_filename = fasta_idx + ".pdb.simrna" #Get tertiary structure filename (will be created)
+    with open(fasta_idx + "mpdr-rna.log","w") as main_logfile:
         with open(fasta_seq_filename,"r") as f1: #Open up the fasta filename
             seq = f1.read().splitlines()[1] #Read the sequence
             with open(fasta_seq_simrna_filename,"w") as f2: #Open up the new SimRNA compliant fasta file
@@ -106,9 +100,8 @@ def rna_tertiary_structure_prediction(filename):
         main_logfile.flush()
         start = time.time()
         if fasta_terstr_filename not in os.listdir(cwd): #If that name doesn't exist
-            with open(fasta_terstr_logfile_filename,"w") as log: #Open the logfile
-                args = ["SimRNA","-E","8","-s",fasta_seq_simrna_filename,"-S",fasta_secstr_simrna_filename,"-c","config.dat","-o",fasta_terstr_filename]
-                call(args,stdout = log) #Calling the terminal command
+            args = ["SimRNA","-E","8","-s",fasta_seq_simrna_filename,"-S",fasta_secstr_simrna_filename,"-c","config.dat","-o",fasta_terstr_filename]
+            call(args) #Calling the terminal command
             main_logfile.write("Initial structure prediction finished, performing clustering." + "\n")
             main_logfile.flush()
         with open(fasta_idx + ".for_clustering.simrna","w") as f1: #Open the file for clustering
@@ -118,13 +111,10 @@ def rna_tertiary_structure_prediction(filename):
                     main_logfile.flush()
                     with open(filename,"r") as f2: #Read them in
                         f1.write(f2.read()) #Paste them in a cat way
-        angstrom_cutoff = "4.4"
-        fraction_to_cluster = "0.01"
         main_logfile.write(("Clustering the top " + fraction_to_cluster + " of each replicate using a " + angstrom_cutoff + " Angstrom cutoff" + "\n"))
         main_logfile.flush()
-        with open(fasta_clustering_logfile_filename,"w") as log: #Create a log file
-            args = ["clustering",fasta_idx + ".for_clustering.simrna",fraction_to_cluster,angstrom_cutoff] #Arguments for clustering
-            call(args,stdout = log) #Perform the actual clustering
+        args = ["clustering",fasta_idx + ".for_clustering.simrna",fraction_to_cluster,angstrom_cutoff] #Arguments for clustering
+        call(args) #Perform the actual clustering
         main_logfile.write("Refining the PDB file, using the clustering runs" + "\n")
         main_logfile.flush()
         for filename in sorted(os.listdir(cwd)): #Iterate one last time over the directory
@@ -155,6 +145,23 @@ def rna_tertiary_structure_refinement(filename):
             main_logfile.write("QRNAS is complete. It took:" + str(round((time.time() - start),0)) + "seconds for" + fasta_idx + "\n")
             main_logfile.flush()
     return(fasta_idx)
+
+def secondary_from_tertiary(filename):
+    fasta_idx = filename.replace(".fasta","") #Get index
+    secondary_structure = "" #Create variable
+    pdb_filename = fasta_idx + ".pdb" #Get the pdb file
+    args = ["x3dna-dssr","i="+pdb_filename] #Create the arguments for DSSR
+    subprocess.call(args) #Run it
+    for file in os.listdir(cwd): #Iterate over the directory
+        if file.startswith("dssr"): #If the files are from dssr
+            if file == "dssr-2ndstrs.dbn": #If it is that specific file
+                with open("dssr-2ndstrs.dbn","r") as f: #Open it up
+                    secondary_structure = f.read().splitlines()[2] #Get the dot bracket notation
+            args = ["rm",file] #Remove all dssr created files anyway
+            subprocess.call(args) #Call it
+    with open(fasta_idx + ".secfromtert","w") as f: #Open a new kind of file
+        f.write(secondary_structure) #Write the structure in
+    return(secondary_structure) #Return variable
 
 def dot_bracket_to_cns(secondary_structure,filename):
     fasta_idx = filename.replace(".fasta","") #Get index
@@ -221,41 +228,6 @@ def change_runcns(filename):
         f.write(text)
         f.flush()
     return(None)
-            
-def clean():
-    #Just clean all files by looping through the working directory
-    for file in os.listdir(cwd):
-        if file.endswith(".def") and file != "dna-rna_restraints_prototype.def":
-            os.remove(file)
-        elif file.endswith(".secfromtert"):
-            os.remove(file)
-        elif file.endswith(".cns") and file!= "run_prototype.cns":
-            os.remove(file)
-        elif file.endswith(".pdb.haddock"):
-            os.remove(file)
-        elif file.endswith(".html") and file != "new_prototype.html":
-            os.remove(file)
-        elif os.path.isdir(file):
-            shutil.rmtree(file)
-        
-    return(None)
-
-def secondary_from_tertiary(filename):
-    fasta_idx = filename.replace(".fasta","") #Get index
-    secondary_structure = "" #Create variable
-    pdb_filename = fasta_idx + ".pdb" #Get the pdb file
-    args = ["x3dna-dssr","i="+pdb_filename] #Create the arguments for DSSR
-    subprocess.call(args) #Run it
-    for file in os.listdir(cwd): #Iterate over the directory
-        if file.startswith("dssr"): #If the files are from dssr
-            if file == "dssr-2ndstrs.dbn": #If it is that specific file
-                with open("dssr-2ndstrs.dbn","r") as f: #Open it up
-                    secondary_structure = f.read().splitlines()[2] #Get the dot bracket notation
-            args = ["rm",file] #Remove all dssr created files anyway
-            subprocess.call(args) #Call it
-    with open(fasta_idx + ".secfromtert","w") as f: #Open a new kind of file
-        f.write(secondary_structure) #Write the structure in
-    return(secondary_structure) #Return variable
 
 def prepare_haddock(filename):
     fasta_idx = filename.replace(".fasta","") #Get index
@@ -289,25 +261,67 @@ def prepare_haddock(filename):
     elif password == "":
         args = ["sudo","python2",haddock_dir+"/Haddock/RunHaddock.py"]
         subprocess.call(args)
-    os.chdir("..")
+    os.chrdir("structures/it1/water")
+    args = ["sudo","-S",password,"python2",haddock_dir+"tools/ana_structures.csh"]
+    call(args)
+    with open("structures_haddock-sorted.stat","r") as f:
+        lines = f.read().splitlines()
+        best_struct = lines[1]
+        best_struct_name,best_struct_score = best_struct.split(" ")[0:2]
+    os.chdir("../../../..")
+    with open("results.txt","w") as f:
+        f.write(best_struct_name+"\t"+best_struct_score)
+        f.flush()
     return(None)  
-  
+
+def clean():
+    #Just clean all files by looping through the working directory
+    for file in os.listdir(cwd):
+        if file.endswith(".def") and file != "dna-rna_restraints_prototype.def":
+            os.remove(file)
+        elif file.endswith(".secfromtert"):
+            os.remove(file)
+        elif file.endswith(".cns") and file!= "run_prototype.cns":
+            os.remove(file)
+        elif file.endswith(".pdb.haddock"):
+            os.remove(file)
+        elif file.endswith(".html") and file != "new_prototype.html":
+            os.remove(file)
+        elif os.path.isdir(file):
+            shutil.rmtree(file)
+        
+    return(None)
+
+def clean_gen_one():
+    filenames = list(range(1,10))
+    filenames = [str(x) for x in filenames]
+    for file in os.listdir(cwd):
+        if file[0] in filenames:
+            os.remove(file)
+
 #Now all the secondary structure stuff is done, time to reiterate over the directory to calculate the tertiary structure
 if __name__ == "__main__":
     num_threads = 10
-    filenames = []
-    for filename in sorted(os.listdir(cwd)): #Iterate over files in directory
-        if filename.endswith(".fasta"): #If they are fasta
-            filenames.append(filename)
-    print("These are the filenames for which tertiary structure prediction will be performed")
-    print(" ".join(filenames))
-    with Pool(num_threads) as pool:
-        pool.map(rna_tertiary_structure_prediction,filenames)
-    with Pool(num_threads) as pool:
-        pool.map(rna_tertiary_structure_refinement,filenames)
-
-# In[ ]:
-
-
-
-
+    #Setting up gen one
+    with open("0.fasta","r") as f: #Open up the original file
+        first_seq = f.read().splitlines()[1] #Obtain sequence
+    (ss,mfe) = RNA.fold(first_seq) #Fold it
+    with open("0.secstr","w") as f: #Write secondary structure file
+        f.write(">0 " + str(mfe)) #Right energy
+        f.write("\n")
+        f.write(ss) #Write sequence
+    for i in range(9): #Create 9 offspring from the first file
+        max_index = get_max_index() #Get the max index each time, just to be sure that the creation is going fine
+        mutate_seq_and_get_secondary_structure("0.fasta",max_index) #Create .fasta and .secstr files for them
+    filenames = list(range(0,10)) #Get the filenames from the first generation (those are set)
+    filenames = [str(x) + ".fasta" for x in filenames] #Get the actual fasta name (not that it really matters)
+    with Pool(num_threads) as pool: #Thread pool 1
+        pool.map(rna_tertiary_structure_prediction,filenames) #Predict tertiary structure
+    with Pool(num_threads) as pool: #Thread pool 2 (in order to synchronise the two)
+        pool.map(rna_tertiary_structure_refinement,filenames) #Refine tertiary structure
+    secondary_structures = list(map(secondary_from_tertiary,filenames)) #This takes too little to need pooling, get secondary structure from tertiary
+    list(map(dot_bracket_to_cns,secondary_structures,filenames)) #Create ten separate rna-dna_restraints.def files, one for each
+    list(map(change_runcns,filenames)) #Create ten separate run.cns files, one for each
+    list(map(edit_pdb_for_haddock_compliance,filenames)) #Edit all filenames to make sure they are HADDOCK compliant
+    with Pool(cores) as pool:
+        pool.map(prepare_haddock,candidate) #Run docking
