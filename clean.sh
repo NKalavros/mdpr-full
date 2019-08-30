2957bbc98f428e795ca1508de309d9c5ed84dd3e
NKalavros <39969236+NKalavros@users.noreply.github.com>
2019-08-30 03:00:59 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,28 @@
 mv 0.fasta ..
+mv crp_monomer.pdb ..
+mv run_prototype.cns ..
+mv new_prototype.html ..
+mv dna-rna_restraints_prototype.def ..
+
+
+
 rm *.fasta
-rm *.trafl
 rm *.secstr
-rm *.pdb
 rm *.simrna*
-rm *qrna*
-rm *fastampdr*
+rm *.trafl
 rm *ss_detected
+rm *qrna*
+rm *mdpr-rna.log
+rm *.pdb
+
+
+rm *.secfromtert
+rm *.pdb.haddock
+rm *.def
+rm*.cns
+
 mv ../0.fasta .
+mv ../cpr_monomer.pdb .
+mv ../run_prototype.cns .
+mv ../new_prototype.html .
+mv ../dna-rna_restraints_prototype .
